4e144c7ed59ae522369becbdfa511a2e98efbabd
elfcoda <nanohnu@gmail.com>
2024-02-07 00:37:15 +0800

    update style

--- src/Compnent/chart.js
+++ src/Compnent/chart.js
@@ -19,8 +19,9 @@ function Chart() {
         // 在 useEffect 中进行网络请求
         const fetchData = async () => {
           try {
-            const response = await fetch('http://127.0.0.1:8000/test/');
-            
+            // const response = await fetch('http://127.0.0.1:8000/test/');
+            const response = await fetch('http://127.0.0.1:8000/analyse/');
+
             if (!response.ok) {
               throw new Error('Network request failed');
             }
@@ -28,7 +29,7 @@ function Chart() {
             const data = await response.json();
     
             // 将获取的数据设置到状态中
-            setApiData([...data["neg"], ...data["pos"]]);
+            setApiData([...data["short"], ...data["long"]]);
           } catch (error) {
             console.error('Error fetching data:', error.message);
           }
@@ -38,6 +39,7 @@ function Chart() {
         fetchData();
       }, []);
 
+
     useEffect(() => {
         if (apiData && apiData.length !== 0) {
 
@@ -45,8 +47,16 @@ function Chart() {
                 // 初始化图表
                 const chart = init('chart' + index)
 
-                const candleSticks = kLine.map((stick) => 
-                ({
+                const candleSticks = kLine[0].map((stick, index) => 
+                ((index === kLine[1] || index === kLine[2]) ? {
+                    timestamp: parseFloat(stick[HISTORY_CANDLES_TS]),
+                    open: parseFloat(stick[HISTORY_CANDLES_OPEN]),
+                    high: parseFloat(stick[HISTORY_CANDLES_HIGH]),
+                    low: parseFloat(stick[HISTORY_CANDLES_LOW]) - 1000, // TODO
+                    close: parseFloat(stick[HISTORY_CANDLES_CLOSE]),
+                    volume: parseFloat(stick[HISTORY_CANDLES_VOL]),
+                } :
+                {
                     timestamp: parseFloat(stick[HISTORY_CANDLES_TS]),
                     open: parseFloat(stick[HISTORY_CANDLES_OPEN]),
                     high: parseFloat(stick[HISTORY_CANDLES_HIGH]),
@@ -60,25 +70,26 @@ function Chart() {
                 chart.createIndicator("MACD")
             });
             
+            
             return () => {
                 // 销毁图表
                 apiData.forEach((kLine, index) => {
                     dispose('chart' + index)
                 });
-              }
+            }
         }
       }, [apiData])
 
     return (
         <>
-            <div>{ "apiData" }</div>
-            {
-                apiData && apiData.length !== 0 && (apiData.map((kLine, index) => (
-                    <div key={index}>
-                        <div id={`chart${index}`} style={{ width: 400, height: 400 }}/>
-                    </div>
-                )))
-            }
+            <div>{ "apiData: " } { apiData ? apiData.length : 0 }</div>
+            <div style={{  }}>
+                {
+                    apiData && apiData.length !== 0 && (apiData.map((kLine, index) => (
+                        <div key={index} id={`chart${index}`} style={{ width: 400, height: 400, display: "inline-block", margin: "10px" }}/>
+                    )))
+                }
+            </div>
         </>
         
     );
